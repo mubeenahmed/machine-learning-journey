9b14cfce2ec5c2fadc147ee89640db87d10c35ba
= <=>
2018-09-22 11:58:53 +0500

    shape detection

---
+++ application/contour_shape.py
@@ -0,0 +1,79 @@
+# import the necessary packages
+import argparse
+import imutils
+import cv2
+
+class ShapeDetector:
+	def __init__(self):
+		pass
+
+	def detect(self, c):
+		# initialize the shape name and approximate the contour
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+		# if the shape is a triangle, it will have 3 vertices
+		if len(approx) == 3:
+			shape = "triangle"
+
+		# if the shape has 4 vertices, it is either a square or
+		# a rectangle
+		elif len(approx) == 4:
+			# compute the bounding box of the contour and use the
+			# bounding box to compute the aspect ratio
+			(x, y, w, h) = cv2.boundingRect(approx)
+			ar = w / float(h)
+
+			# a square will have an aspect ratio that is approximately
+			# equal to one, otherwise, the shape is a rectangle
+			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+
+		# if the shape is a pentagon, it will have 5 vertices
+		elif len(approx) == 5:
+			shape = "pentagon"
+
+		# otherwise, we assume the shape is a circle
+		else:
+			shape = "circle"
+
+		# return the name of the shape
+		return shape
+		
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, blur it slightly,
+# and threshold it
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 1)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+# find contours in the thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+sd = ShapeDetector()
+
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+
+	shape = sd.detect(c)
+	# draw the contour and center of the shape on the image
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+	cv2.putText(image, shape, (cX - 20, cY - 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+	# show the image
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
+ 
